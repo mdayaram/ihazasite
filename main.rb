25291adf07055bd9e9b6573a4e7f3bdecca9fbf2
Manoj Dayaram <noj@moovweb.com>
2012-12-28 20:59:51 -0800

    new title, adding config.ru

--- main.rb
+++ main.rb
@@ -6,8 +6,9 @@ require 'glorify'
 # Config for markdown layout engine.
 Tilt.prefer Sinatra::Glorify::Template, '.md', '.markdown', '.mkd'
 set :markdown, { :layout_engine => :haml, :layout => :'layouts/markdown' }
-#register Sinatra::Glorify  #only in modular apps
+# If using modular apps, need to look up how Glorify and sinatra-content-for work
 
+# Routes
 get "/" do
   markdown(:hugs)
 end
