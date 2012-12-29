e72e90add1926a412930161c2e02ee3fd2007fe8
Manoj Dayaram <noj@moovweb.com>
2012-12-28 23:04:25 -0800

    Dynamic css!

--- main.rb
+++ main.rb
@@ -1,12 +1,24 @@
+require 'rubygems'
 require 'sinatra'
 
 # Markdown renderer
 require 'glorify'
+require 'haml'
+require 'sass'
 
 # Config for markdown layout engine.
 Tilt.prefer Sinatra::Glorify::Template, '.md', '.markdown', '.mkd'
 set :markdown, { :layout_engine => :haml, :layout => :'layouts/markdown' }
 # If using modular apps, need to look up how Glorify and sinatra-content-for work
+
+# Stylesheets
+get "/stylesheets/:sheet.css" do |sheet|
+  content_type 'text/css', :charset => 'utf-8'
+  if sheet.nil? or sheet.empty?
+    return 404
+  end
+  scss "stylesheets/#{sheet}".to_sym, :charset => "utf-8"
+end
 
 # Routes
 get "/" do
